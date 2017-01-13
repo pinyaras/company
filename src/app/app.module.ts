52af80865ad18743a4bd18f874710e94770ea906
Pinyarash Pinyoanunpong <Pinyarash Pinyoanunpong>
2017-01-13 09:17:41 -0800

    modified:   angular-cli.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/index.html 	src/app/about/ 	src/app/bestseller/ 	src/app/category/ 	src/app/login/ 	src/app/news/ 	src/app/newsall/ 	src/app/product/ 	src/app/shared/ 	src/app/slide/ 	src/app/staff/ src/assets/css/ 	src/assets/images/ 	src/assets/js/ 	src/css/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { BestsellerComponent } from './bestseller/bestseller.component';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { AboutComponent } from './about/about.component';
+import { StaffComponent } from './staff/staff.component';
+import { NewsallComponent } from './newsall/newsall.component';
+import { NewsService } from './shared/news.service';
+import { ProductService } from './shared/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryComponent,
+    BestsellerComponent,
+    NewsComponent,
+    LoginComponent,
+    SlideComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    AboutComponent,
+    StaffComponent,
+    NewsallComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: NewsComponent},
+      { path: 'product', component: ProductComponent},
+      { path: 'product/:id', component: ProductDetailComponent},
+      { path: 'staff', component: StaffComponent},
+      { path: 'newsall', component: NewsallComponent},
+      { path: 'about', component: AboutComponent},
+      { path: '**', redirectTo: ''}
+    ], {useHash: true})
   ],
-  providers: [],
+  providers: [Title, NewsService, ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
